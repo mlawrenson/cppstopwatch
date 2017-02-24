d7e335191ed22ce1157017e09313e27c8bfeb378
Mark Lawrenson <mark@lawrenson.id.au>
2017-02-24 18:13:06 +1100

    Improve tests

--- src/test/test.cc
+++ src/test/test.cc
@@ -10,17 +10,23 @@ int main(int argc, char** argv)
   Stopwatch swatch;
   swatch.set_mode(REAL_TIME);
 
-  swatch.start("3sec");
+  swatch.start("2sec");
   std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-  swatch.pause("3sec");
+  swatch.pause("2sec");
 
   swatch.start("1sec");
   std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   swatch.stop("1sec");
 
-  swatch.start("3sec");
-  std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-  swatch.stop("3sec");
+  for (int i = 0; i < 10; i++) {
+    swatch.start("10x0.1s");
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    swatch.stop("10x0.1s");
+  }
+
+  swatch.start("2sec");
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+  swatch.stop("2sec");
 
   swatch.report_all();
   return 0;
